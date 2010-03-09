7fec8f895783cb85bdee004213b7cbb717b757a1
Arthur Schreiber <schreiber.arthur@gmail.com>
2010-03-09 22:52:25 +0100

    Added some comments.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,15 @@
+// wait till the page has loaded before observing events
 document.observe("dom:loaded", function() {
+  
+  // observe keypress events
   document.observe("keypress", function(event) {
     
+    // top and left represent the x and y position
+    // of our ship from the top left corner.
     var top = (parseInt($("ship").style.top) || 0),
       left = (parseInt($("ship").style.left) || 0);
     
+    // Move the ship based on the pressed keys
     switch (event.keyCode) {
       case 37: // Left arrow
         left--;
@@ -22,9 +28,8 @@ document.observe("dom:loaded", function() {
         break;
     }
     
-    
+    // Move the ship to it's new position.
     $("ship").style.top = top + "px";
     $("ship").style.left = left + "px";
-    
   });
 });
