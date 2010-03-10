f2eb3fec8d871c17f610c10cfb52fc1e3055b2b3
Arthur Schreiber <schreiber.arthur@gmail.com>
2010-03-10 21:29:14 +0100

    Add a basic Key state manager and some unit tests for it.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,30 @@
+var Key = (function() {
+  var pressed = {};
+  
+  function isDown(keyCode) {
+    return pressed[keyCode] === true;
+  }
+  
+  function down(keyCode) {
+    pressed[keyCode] = true;
+  }
+  
+  function up(keyCode) {
+    delete pressed[keyCode];
+  }
+  
+  return {
+    LEFT:  37,
+    UP:    38,
+    RIGHT: 39,
+    DOWN:  40,
+    
+    isDown: isDown,
+    down:   down,
+    up:     up
+  };
+})();
+
 // wait till the page has loaded before observing events
 document.observe("dom:loaded", function() {
   
