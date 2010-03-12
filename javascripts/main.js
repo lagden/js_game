fc5ac7649ab0c15816baba03a2b3cb62aaa1bca2
Arthur Schreiber <schreiber.arthur@gmail.com>
2010-03-12 11:50:39 +0100

    Extracted the Key object into a seperate file.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,30 +1,3 @@
-var Key = (function() {
-  var pressed = {};
-  
-  function isDown(keyCode) {
-    return pressed[keyCode] === true;
-  }
-  
-  function down(keyCode) {
-    pressed[keyCode] = true;
-  }
-  
-  function up(keyCode) {
-    delete pressed[keyCode];
-  }
-  
-  return {
-    LEFT:  37,
-    UP:    38,
-    RIGHT: 39,
-    DOWN:  40,
-    
-    isDown: isDown,
-    down:   down,
-    up:     up
-  };
-})();
-
 // Wait till the page has loaded and start the game
 document.observe("dom:loaded", Game.initialize.bind(Game));
 
