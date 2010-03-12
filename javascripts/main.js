876c29be30801516a84587ac404dfe810d087bd8
Arthur Schreiber <schreiber.arthur@gmail.com>
2010-03-12 11:41:34 +0100

    Move #draw and #tick from global namespace into a Game object.

--- javascripts/main.js
+++ javascripts/main.js
@@ -25,49 +25,59 @@ var Key = (function() {
   };
 })();
 
-function draw() {
-  var ship = $("ship"),
-    left = parseInt(ship.style.left) || 0,
-    top = parseInt(ship.style.top) || 0;
+var Game = (function() {
+  var ship;
   
-  if (Key.isDown(Key.LEFT)) {
-    left--;
-  }
-  if (Key.isDown(Key.RIGHT)) {
-    left++;
-  }
-  if (Key.isDown(Key.UP)) {
-    top--;
-  }
-  if (Key.isDown(Key.DOWN)) {
-    top++;
+  function initialize() {
+    // Create and insert the Ship element
+    ship = new Element("div", { id: "ship" });
+    $("canvas").insert(ship);
+    
+    tick();
   }
   
-  ship.style.left = left + "px";
-  ship.style.top = top + "px";
-}
+  function tick() {
+    draw();
+    setTimeout(tick, 20);
+  }
+  
+  function draw() {
+    var left = parseInt(ship.style.left) || 0,
+        top = parseInt(ship.style.top) || 0;
+    
+    if (Key.isDown(Key.LEFT)) {
+      left--;
+    }
+    if (Key.isDown(Key.RIGHT)) {
+      left++;
+    }
+    if (Key.isDown(Key.UP)) {
+      top--;
+    }
+    if (Key.isDown(Key.DOWN)) {
+      top++;
+    }
+    
+    ship.style.left = left + "px";
+    ship.style.top = top + "px";
+  }
+  
+  return {
+    initialize: initialize,
+    draw:       draw,
+    tick:       tick
+  };
+}());
 
-function tick() {
-  draw();
-  setTimeout(tick, 20);
-}
+// Wait till the page has loaded and start the game
+document.observe("dom:loaded", Game.initialize.bind(Game));
 
-// wait till the page has loaded before observing events
-document.observe("dom:loaded", function() {
-  
-  // Create and insert the Ship element
-  var ship = new Element("div", { id: "ship" });
-  $("canvas").insert(ship);
-  
-  // observe keypress events
-  document.observe("keydown", function(event) {
-    Key.down(event.keyCode);
-  });
-  
-  document.observe("keyup", function(event) {
-    Key.up(event.keyCode);
-  });
-  
-  // Start the timer
-  tick();
+// Observe keydown events
+document.observe("keydown", function(event) {
+  Key.down(event.keyCode);
+});
+
+// Observe keyup events
+document.observe("keyup", function(event) {
+  Key.up(event.keyCode);
 });
