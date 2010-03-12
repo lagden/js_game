bc773836d09f8b61120b90b44d7ac29ed05afa59
Arthur Schreiber <schreiber.arthur@gmail.com>
2010-03-12 12:17:37 +0100

    Added a moving background image.

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,11 +1,16 @@
 var Game = (function() {
-  var ship;
+  var ship, background;
   
   function initialize() {
+    // Create and insert the background element
+    background = new Element("div", { id: "background" });
+    $("canvas").insert(background);
+
     // Create and insert the Ship element
     ship = new Element("div", { id: "ship" });
     $("canvas").insert(ship);
     
+    // Start the tick loop
     this.tick();
   }
   
@@ -15,6 +20,7 @@ var Game = (function() {
   }
   
   function draw() {
+    // First, move the ship
     var left = parseInt(ship.style.left) || 0,
         top = parseInt(ship.style.top) || 0;
     
@@ -33,6 +39,14 @@ var Game = (function() {
     
     ship.style.left = left + "px";
     ship.style.top = top + "px";
+    
+    // Now move the background
+    left = parseInt(background.style.left) || 0;
+    left -= 1;
+    if (left < -2110) {
+      left = 0;
+    } 
+    background.style.left = left + "px";
   }
   
   return {
